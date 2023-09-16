c88ac04a8387b22d9760962db324b98d8699fba1
shinhong-park <shinhong.park@linecorp.com>
2023-09-16 20:27:47 +0900

    Create seminar1 practice project

---
+++ ios/seminar1/Seminar1Practice/Seminar1Practice/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Seminar1Practice
+//
+//  Created by user on 2023/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
