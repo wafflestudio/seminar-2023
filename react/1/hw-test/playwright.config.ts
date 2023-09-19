50872da2793c9e6bd5523d3e73853f6015b002fe
woohm402 <woohm404@gmail.com>
2023-09-19 13:03:26 +0900

    install playwright

---
+++ react/1/hw-test/playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const CI = true;
+const PORT = 5173;
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./e2e",
+  fullyParallel: true,
+  forbidOnly: CI,
+  retries: CI ? 2 : 0,
+  workers: CI ? 4 : undefined,
+  reporter: CI ? "dot" : "html",
+  use: {
+    video: "retain-on-failure",
+    baseURL: `http://localhost:${PORT}`,
+    trace: "retain-on-failure",
+    permissions: ["clipboard-read"],
+  },
+  projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"] } }],
+});
